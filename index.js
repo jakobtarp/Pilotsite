9c5ca947a716de6281574f85325cc3623d2976c4
jakobtarp <jakob.tarp@hotmail.com>
2022-05-16 13:44:45 +0200

    Version2

--- index.js
+++
@@ -1,9 +0,0 @@
-const knap = document.querySelector("button");
-
-const div = document.querySelector("div");
-
-function klikket() {
-  div.classList.toggle("min-klasse");
-}
-
-knap.addEventListener("click", klikket);
